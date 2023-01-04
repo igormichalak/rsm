f69479500ac005e4d7d2fedae3c3843b09adfc12
Igor Michalak <git@igormichalak.com>
2023-01-04 19:39:13 +0100

    feat: initializing sessions

--- manager.go
+++ manager.go
@@ -22,7 +22,19 @@ func (sm *SessionManager) InitSession() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return token, nil
+	s := &session{
+		token:  token,
+		values: make(map[string]any),
+		expiry: time.Now().Add(sm.Lifetime).UTC(),
+	}
+	data, err := s.encodeData()
+	if err != nil {
+		return "", err
+	}
+	if err = sm.store.Insert(s.token, data, s.expiry); err != nil {
+		return "", err
+	}
+	return s.token, nil
 }
 
 func (sm *SessionManager) RetrieveValues(token string) (map[string]any, error) {
@@ -33,9 +45,8 @@ func (sm *SessionManager) RetrieveValues(token string) (map[string]any, error) {
 	if err != nil {
 		return nil, err
 	}
-	s := session{token: token}
-	err = s.decodeData(data)
-	if err != nil {
+	s := &session{token: token}
+	if err = s.decodeData(data); err != nil {
 		return nil, err
 	}
 	return s.values, nil
